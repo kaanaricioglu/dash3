2f064bf0694d04b8d7993e606ca6f58fa2378c7f
Kaan Arıcıoğlu <kaan.aricioglu@hisarschool.k12.tr>
2024-10-03 10:30:16 -0400

    Fix missing load_dotenv import

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, Response, stream_with_context
 import datetime
 import requests
+from dotenv import load_dotenv
 import os
 import json
 import logging
